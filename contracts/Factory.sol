3d956e7c9dbcf4e5080a3d1b096badaf869a4982
Florian-Haas <fhaasbusiness@gmail.com>
2025-02-15 13:03:48 +0100

    Write tests

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-contract Factory {}
+contract Factory {
+    string public name = "Factory";
+    string public name2 = "Factory";
+}
--- test/Factory.js
+++ test/Factory.js
@@ -1,7 +1,32 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Factory", function () {
+  async function deployFactoryFixture() {
+    // Fetch the contract
+    const Factory = await ethers.getContractFactory("Factory");
+    // Deploy the contract
+    const factory = await Factory.deploy();
 
-})
+    return { factory };
+  }
+
+  it("should have a name", async function () {
+    const { factory } = await deployFactoryFixture();
+    // Check name
+    const name = await factory.name();
+    // Check name is correct
+    expect(name).to.equal("Factory");
+  });
+
+  it("should have another name", async function () {
+    const { factory } = await deployFactoryFixture();
+    // Check name
+    const name = await factory.name2();
+    // Check name is correct
+    expect(name).to.equal("Factory2");
+  });
+});
