13a98b2c871e88920d7cf4d657049b3ac804d0d0
Florian-Haas <fhaasbusiness@gmail.com>
2025-02-18 14:58:02 +0100

    Finish Header Component, to connect to Metamask

--- app/page.js
+++ app/page.js
@@ -30,15 +30,12 @@ export default function Home() {
   }
 
   useEffect(() => {
-    loadBlockchainData(), [];
-  });
+    loadBlockchainData();
+  }, []);
 
   return (
     <div className="page">
-      <Header
-        account={0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266}
-        setAccount={""}
-      />
+      <Header account={account} setAccount={setAccount} />
     </div>
   );
 }
